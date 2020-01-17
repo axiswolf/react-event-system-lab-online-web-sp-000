ba9eeaf4ad0b53f7c50b4328ea0e8d92733de2fe
Karen V <karenvo@usc.edu>
2020-01-17 08:58:55 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,9 +2,14 @@
 import React from 'react';
 
 class KeyPad extends React.Component {
+  enterPassword = () => console.log('Entering password...')
   render() {
     // return more than one input
     // need event handler (KeyUp)
-    // then console.log('entering password...')
+    return (
+      <div>
+        <input type="password" onKeyUp={this.enterPassword} />
+      </div>
+    )
   }
 }
