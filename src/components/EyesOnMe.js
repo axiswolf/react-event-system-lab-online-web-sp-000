b01695056c9eb4537c535eb27c8d5d2216cccb97
Karen V <karenvo@usc.edu>
2020-01-17 09:03:55 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,17 +1,14 @@
-// Code EyesOnMe Component Here
 import React from 'react';
 
 class EyesOnMe extends React.Component {
-  // render button
-  // event listeners for focus and blur
-  // focus => console.log('Good!')
-  // blur => console.log(`Hey! Eyes on me!`)
-  onFocus = () => console.log(`Good!`);
-  onBlur = () => console.log(`Hey! Eyes on me!`)
+
+  onFocus = () => console.log('Good!');
+  onBlur = () => console.log('Hey! Eyes on me!')
+
   render() {
     return (<button onFocus={this.onFocus} onBlur={this.onBlur}>
-      Eyes on me, please!
-      </button>
+        Eyes on me, please!
+    </button>
     )
   }
 }
